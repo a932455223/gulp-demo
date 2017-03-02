5cafa0b7c4e4f2e0218de56fe5022a6d7c2adbad
simon.du <a932455223@qq.com>
2017-03-02 16:30:28 +0800

    less copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 let swig = require('gulp-swig');
 let gulp = require('gulp');
 let browserSync = require('browser-sync').create();
-let sass = require('gulp-sass');
+let less = require('gulp-less');
+let copy = require('gulp-contrib-copy');
 
 gulp.task('templates',function(){
 	gulp.src('./src/view/*.swig')
@@ -14,7 +15,7 @@ gulp.task('templates',function(){
 });
 
 
-gulp.task('serve',['sass','templates'], function() {
+gulp.task('serve',['less','templates'], function() {
     browserSync.init({
         server: {
             baseDir: 'dist/'
@@ -22,18 +23,35 @@ gulp.task('serve',['sass','templates'], function() {
         startPath:'/view/index.html'
     });
 
-    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/less/*.less", ['less']);
 
     gulp.watch("src/view/**/*.swig", ['templates',browserSync.reload]);
+
+    gulp.watch("src/js/*.js", ['copyJs']);
+
+    gulp.watch("src/fonts/*", ['copyFt']);
 });
 
 
-gulp.task('sass', function () {
+gulp.task('less', function () {
 
-   return gulp.src("src/scss/*.scss")
-        .pipe(sass())
+   return gulp.src("src/less/*.less")
+        .pipe(less())
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
+});
+
+gulp.task('copyJs',function(){
+    return gulp.src("src/js/*")
+           .pipe(copy())
+           .pipe(gulp.dest('dist/js/'))
+});
+
+
+gulp.task('copyFt',function(){
+    return gulp.src("src/fonts/*")
+           .pipe(copy())
+           .pipe(gulp.dest('dist/fonts/'))
 });
 
 gulp.task('default', ['serve']);
