e083cd0a729bada124ddddeea2223df9a86a1352
simon.du <a932455223@qq.com>
2017-03-06 13:08:57 +0800

    add gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,40 @@ let copy = require('gulp-contrib-copy');
 let autoprefixer = require('autoprefixer'); 
 let cssnano = require('cssnano');
 let postcss = require('gulp-postcss');
-
-gulp.task('templates',function(){
-	gulp.src('./src/view/*.swig')
-	.pipe(swig({
-        defaults:{
-            cache:false
-        }
-    }))
-	.pipe(gulp.dest('./dist/view/'))
-});
+var plumber = require('gulp-plumber');
 
 
-gulp.task('serve',['less','templates'], function() {
+
+
+gulp.task('serve',['less','templates','copyJs','copyFt'], function() {
     browserSync.init({
         server: {
             baseDir: 'dist/'
         },
-        startPath:'/view/index.html'
+        startPath:'/view/index.html',
+        // open:false
     });
 
     gulp.watch("src/less/*.less", ['less']);
 
     gulp.watch("src/view/**/*.swig", ['templates',browserSync.reload]);
 
-    gulp.watch("src/js/*.js", ['copyJs',browserSync.reload]);
+    gulp.watch("src/js/**", ['copyJs',browserSync.reload]);
 
-    gulp.watch("src/fonts/*", ['copyFt']);
+    gulp.watch("src/fonts/**", ['copyFt']);
+    gulp.watch("src/images/**", ['copyImg']);
+});
+
+
+gulp.task('templates',function(){
+  gulp.src('./src/view/*.swig')
+  .pipe(plumber())
+  .pipe(swig({
+        defaults:{
+            cache:false
+        }
+    }))
+  .pipe(gulp.dest('./dist/view/'))
 });
 
 
@@ -40,7 +47,8 @@ gulp.task('less', function () {
 
   let processors = [ autoprefixer({browsers:'last 2 version'}), cssnano ];
 
-   return gulp.src("src/less/*.less")
+   return gulp.src(["src/less/common.less","src/less/index.less"])
+        .pipe(plumber())
         .pipe(less())
         .pipe(postcss(processors))
         .pipe(gulp.dest("dist/css"))
@@ -48,16 +56,22 @@ gulp.task('less', function () {
    });
 
 gulp.task('copyJs',function(){
-    return gulp.src("src/js/*")
+    return gulp.src("src/js/**")
            .pipe(copy())
            .pipe(gulp.dest('dist/js/'))
 });
 
 
 gulp.task('copyFt',function(){
-    return gulp.src("src/fonts/*")
+    return gulp.src("src/fonts/**")
            .pipe(copy())
            .pipe(gulp.dest('dist/fonts/'))
+});
+
+gulp.task('copyImg',function(){
+    return gulp.src("src/images/**")
+           .pipe(copy())
+           .pipe(gulp.dest('dist/images/'))
 });
 
 gulp.task('default', ['serve']);
